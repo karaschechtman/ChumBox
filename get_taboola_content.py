16c3f6bfdcd243e7869f267bf330b4bbf351e070
karaschechtman <kws2121@columbia.edu>
2019-05-14 18:38:25 -0400

    full scraper

--- get_taboola_content.py
+++ get_taboola_content.py
@@ -1,9 +1,16 @@
+from bs4 import BeautifulSoup
 from selenium import webdriver
 from taboola_box import TaboolaBox
 import os
-import requests
-from bs4 import BeautifulSoup
 import re
+import requests
+import csv
+import time
+
+CSV_FOLDER = 'websites/'
+DATA_FOLDER = 'data/'
+URL_IN_EXCEPTIONS = ['news']
+CAP = 10
 
 def _get_regex_from_pattern(pattern):
     pattern = pattern.replace('///', '.*/')
@@ -12,7 +19,8 @@ def _get_regex_from_pattern(pattern):
     pattern = pattern.replace('2d','[0-9]{2}')
     pattern = pattern.replace('2y','[0-9]{2}')
     pattern = pattern.replace('headline','.*')
-    print(pattern)
+    pattern = pattern.replace('section','[a-zA-Z]+')
+    pattern = pattern.replace('?','\?')
     return pattern
 
 '''
@@ -20,55 +28,72 @@ Get the links of all articles on a website that fit a given
 pattern for URLs of news articles.
 '''
 def get_article_links(site, stripped_url, pattern):
-    print(pattern)
-    page = requests.get(site)
-    soup = BeautifulSoup(page.text, "html.parser")
-    pattern = re.compile(_get_regex_from_pattern(pattern))
-    for link in soup.find_all('a', href=True):
-        # Get the components of the URL.
-        url_components = link['href'].split('/')
-        start = 0
-        for i in range(len(url_components)):
-            if stripped_url in url_components[i] or url_components[i] in stripped_url:
-                start = i
-        url_pattern = ('/').join(url_components[start+1:])
-        if pattern.match(url_pattern):
-            print(url_pattern)
+    links = []
+    try:
+        page = requests.get(site)
+        soup = BeautifulSoup(page.text, "html.parser")
+        pattern = re.compile(_get_regex_from_pattern(pattern))
+        for link in soup.find_all('a', href=True):
+            # Get the components of the URL.
+            url_components = link['href'].split('/')
+            start = 0
+            for i in range(len(url_components)):
+                if stripped_url in url_components[i] or url_components[i] in stripped_url \
+                and url_components[i] != '' and url_components[i] not in URL_IN_EXCEPTIONS:
+                    start = i
+            url_pattern = '/' + ('/').join(url_components[start+1:])
+            # print(url_pattern)
+            if pattern.match(url_pattern):
+                links.append(url_pattern)
+        return links
+    except:
+        return []
 
 '''
 Get Taboola content from a page with Taboola advertisements.
 '''
 def get_taboola_content(driver, url):
     parsed_boxes = []
-    driver.get(url)
-    taboola_content = driver.find_elements_by_xpath("//div[starts-with(@id, 'internal_trc_')]")
-    for taboola_row in taboola_content:
-        boxes = taboola_row.find_elements_by_xpath("//div[@data-item-title]")
-        for box in boxes:
-            link = box.find_element_by_css_selector('a').get_attribute("href")
-            text = box.text.split('\n')
-            if len(text) >= 2:
-                parsed_boxes.append(TaboolaBox(text[0], text[1], link, url))
-    return parsed_boxes
-
+    try:
+        driver.get(url)
+        taboola_content = driver.find_elements_by_xpath("//div[starts-with(@id, 'internal_trc_')]")
+        for taboola_row in taboola_content:
+            boxes = taboola_row.find_elements_by_xpath("//div[@data-item-title]")
+            for box in boxes:
+                link = box.find_element_by_css_selector('a').get_attribute("href")
+                text = box.text.split('\n')
+                if len(text) >= 2:
+                    parsed_boxes.append(TaboolaBox(text[0], text[1], link, url))
+        return parsed_boxes
+    except:
+        return parsed_boxes
 
 if __name__  == "__main__":
-    get_article_links('https://www.chron.com/', 'chron.com', '///article/headline')
-    # driver = webdriver.Chrome()
-    # publisher = 'FoxNews'
-    # if not os.path.isdir('data/%s' % publisher):
-    #     os.mkdir('data/%s' % publisher)
-    # url = "https://www.foxnews.com/us/nj-student-dies-after-prom"
-    # parsed_boxes = get_taboola_content(driver, url)
-    # for box in parsed_boxes:
-    #     box.write(publisher)
-    #
-    # publisher = 'FoxNews'
-    # if not os.path.isdir('data/%s' % publisher):
-    #     os.mkdir('data/%s' % publisher)
-    # url = "https://www.foxnews.com/world/iranian-president-aide-trump-war-mustache-john-bolton"
-    # parsed_boxes = get_taboola_content(driver, url)
-    # for box in parsed_boxes:
-    #     box.write(publisher)
-    #
-    # driver.quit()
+    driver = webdriver.Chrome() # create a webdriver.
+
+    for filename in os.listdir(CSV_FOLDER):
+        # Iterate through CSVs with data.
+        if filename.endswith('.csv'):
+            with open(CSV_FOLDER + filename, 'r') as f:
+                reader = csv.reader(f, delimiter=',')
+                for row in reader:
+                    if row[3] == 'TRUE' and row[5]: # Taboola website.
+                        # Get the article links.
+                        big_url = 'https://www.' + row[0]
+                        article_links = get_article_links(big_url, row[0], row[5])
+                        if len(article_links) == 0:
+                            big_url = 'http://www.' + row[0]
+                            if len(get_article_links(big_url, row[0], row[5])) == 0:
+                                print('Could not scrape data for %s' % row[0])
+                        # Scrape the Taboola ads for 10 articles.
+                        publisher = row[0]
+                        if not os.path.isdir('data/%s' % publisher):
+                            os.mkdir('data/%s' % publisher)
+                        if len(article_links) > CAP:
+                            article_links = article_links[0:CAP]
+                        for link_end in article_links:
+                            url = big_url + link_end
+                            parsed_boxes = get_taboola_content(driver, url)
+                            for box in parsed_boxes:
+                                 box.write(publisher)
+    driver.quit()
